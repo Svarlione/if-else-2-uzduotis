9bce0f280940a47d95c162f9e8bece87d9f856ed
Svarlione <ruslan.brigockij@gmail.com>
2023-08-23 22:00:50 +0300

    Add project files.

---
+++ if else 2 uzduotis/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Labas,parasyk kokia siandien diena, aciu");
+string userInput  = Console.ReadLine();
+int usetInputInteger  = Convert.ToInt32(userInput);
+if (usetInputInteger == 1)
+    Console.WriteLine("Pirmadienis");
+else if (usetInputInteger == 2)
+    Console.WriteLine("Antradienis");
+else if (usetInputInteger == 3)
+    Console.WriteLine("Treciadienis");
+else if (usetInputInteger == 4)
+    Console.WriteLine("Ketvertadinis");
+else if (usetInputInteger == 5)
+    Console.WriteLine("Penktadienis");
+else if (usetInputInteger == 6)
+    Console.WriteLine("Sestadienis");
+else if (usetInputInteger == 7)
+    Console.WriteLine("Sekmadienis");
+else
+    Console.WriteLine("Neteisingas dienos numeris");
